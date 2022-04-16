a47310a7ada033b0840f0969b402ea32b53bd71b
Julio <52258148+july12123@users.noreply.github.com>
2022-04-16 04:19:25 -0400

    doc update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-import anilist from './anilist/anilist';
-import mal from './mal/mal';
+import * as aniAnime from './anilist/anime';
+import * as aniCharacter from './anilist/chracter';
+import * as aniManga from './anilist/manga';
+import * as aniUser from './anilist/user';
 
-const anirequest = {
-  anilist,
-  mal,
+const anilist = {
+  anime: aniAnime,
+  character: aniCharacter,
+  manga: aniManga,
+  user: aniUser,
 };
 
-export default anirequest;
+import * as malAnime from './mal/anime';
+import * as malCharacter from './mal/character';
+import * as malManag from './mal/manga';
+
+const mal = {
+  anime: malAnime,
+  character: malCharacter,
+  manga: malManag,
+};
+
+export default { anilist, mal };
